55b1a355ec666e0147c9f7ebb2aef93a282a7b49
Naveen Raj K <knaveenraj725@gmail.com>
2025-10-08 21:10:49 +0530

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBz_CNPcrzc6M1CY5jrkaYaO_05aMTJhow",
+  authDomain: "bus-ticket-system-ca88c.firebaseapp.com",
+  projectId: "bus-ticket-system-ca88c",
+  storageBucket: "bus-ticket-system-ca88c.firebasestorage.app",
+  messagingSenderId: "564400600016",
+  appId: "1:564400600016:web:9fd93b69b7d68f5769ffd5"
+};
+firebase.initializeApp(firebaseConfig);
